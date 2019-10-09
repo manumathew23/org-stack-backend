ef43ab60209f88eafd244ac7e55203c26a5474b1
Tony Thanuvelil <tonythanuvelil@gmail.com>
2019-10-09 22:56:24 +0530

    add DATABASE_ROUTERS

--- questions/models.py
+++ questions/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Blog(models.Model):
+class Questions(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
     description = models.TextField()
-
-    class Meta:
-        abstract = True
